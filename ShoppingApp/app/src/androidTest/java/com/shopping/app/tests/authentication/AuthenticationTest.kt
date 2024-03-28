cd2dafae9d3777cbc3e7518e8fc13834995956d6
jakub.cekala <jakubc@backbase.com>
2024-03-28 14:02:11 +0100

    Implement basic authentication screen test

---
+++ ShoppingApp/app/src/androidTest/java/com/shopping/app/tests/authentication/AuthenticationTest.kt
@@ -0,0 +1,28 @@
+package com.shopping.app.tests.authentication
+
+import androidx.test.filters.LargeTest
+import com.shopping.app.common.authentication.AuthenticationScreen
+import com.shopping.app.data.authentication.AuthenticationTab
+import com.shopping.app.tests.BaseTest
+import org.junit.Test
+
+@LargeTest
+class AuthenticationTest : BaseTest() {
+
+    @Test
+    fun verifyAuthenticationScreenContent() = run {
+        step("Verify authentication screen content - Sign in tab") {
+            AuthenticationScreen {
+                verifyCommonScreenElements()
+                verifySignInScreenElements()
+            }
+        }
+        step("Verify authentication screen content - Sign up tab") {
+            AuthenticationScreen {
+                changeTab(AuthenticationTab.SIGN_UP)
+                //verifyCommonScreenElements()
+                verifySingUpScreenElements()
+            }
+        }
+    }
+}
