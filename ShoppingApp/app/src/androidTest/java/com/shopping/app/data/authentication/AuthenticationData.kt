321e872a6500e6007177d3c6007a76d8dafc6c74
jakub.cekala <jakubcekala1@gmail.com>
2024-03-29 13:26:14 +0100

    Implement credentials validation tests

--- ShoppingApp/app/src/androidTest/java/com/shopping/app/data/authentication/AuthenticationData.kt
+++ ShoppingApp/app/src/androidTest/java/com/shopping/app/data/authentication/AuthenticationData.kt
@@ -1,8 +1,23 @@
 package com.shopping.app.data.authentication
 
-object SignInData {
+object SignInData_Correct {
     const val email = "test@gmail.com"
     const val password = "123456"
+}
+
+object SignInData_Incorrect {
+    const val email = "test@gmail.com"
+    const val password = "123465"
+}
+
+object SignInData_IncorrectEmail {
+    const val email = "test"
+    const val password = "123456"
+}
+
+object SignInData_TooShortPassword {
+    const val email = "test@gmail.com"
+    const val password = "1234"
 }
 
 object SignUpData {
--- ShoppingApp/app/src/androidTest/java/com/shopping/app/tests/authentication/AuthenticationTest.kt
+++ ShoppingApp/app/src/androidTest/java/com/shopping/app/tests/authentication/AuthenticationTest.kt
@@ -4,7 +4,10 @@ import androidx.test.filters.LargeTest
 import com.shopping.app.common.AndroidDialog
 import com.shopping.app.common.authentication.AuthenticationScreen
 import com.shopping.app.data.authentication.AuthenticationTab
-import com.shopping.app.data.authentication.SignInData
+import com.shopping.app.data.authentication.SignInData_Correct
+import com.shopping.app.data.authentication.SignInData_Incorrect
+import com.shopping.app.data.authentication.SignInData_IncorrectEmail
+import com.shopping.app.data.authentication.SignInData_TooShortPassword
 import com.shopping.app.tests.BaseTest
 import org.junit.Test
 
@@ -38,21 +41,60 @@ class AuthenticationTest : BaseTest() {
                 clickCloseButton()
             }
         }
-        step("Verify error message when there is only email: ${SignInData.email} and no password entered") {
+        step("Verify error message when there is only email: ${SignInData_Correct.email} and no password entered") {
             AuthenticationScreen {
-                authenticate(SignInData.email, "")
+                authenticate(SignInData_Correct.email, "")
             }
             AndroidDialog {
                 verifyDialogMessage("Fields cannot be empty!")
                 clickCloseButton()
             }
         }
-        step("Verify error message when there is no email and only password: ${SignInData.password} entered") {
+        step("Verify error message when there is no email and only password: ${SignInData_Correct.password} entered") {
             AuthenticationScreen {
-                authenticate("", SignInData.password)
+                authenticate("", SignInData_Correct.password)
             }
             AndroidDialog {
                 verifyDialogMessage("Fields cannot be empty!")
+                clickCloseButton()
+            }
+        }
+    }
+
+    @Test
+    fun verifyInvalidEmailError() = run {
+        step("Verify error message when there is email with incorrect format: ${SignInData_IncorrectEmail.email}") {
+            AuthenticationScreen {
+                authenticate(SignInData_IncorrectEmail.email, SignInData_IncorrectEmail.password)
+            }
+            AndroidDialog {
+                verifyDialogMessage("Please enter a valid email address!")
+                clickCloseButton()
+            }
+        }
+    }
+
+    @Test
+    fun verifyTooShortPasswordError() = run {
+        step("Verify error message when there is too short password: ${SignInData_TooShortPassword.password}") {
+            AuthenticationScreen {
+                authenticate(SignInData_TooShortPassword.email, SignInData_TooShortPassword.password)
+            }
+            AndroidDialog {
+                verifyDialogMessage("Password must be greater than 5 characters!")
+                clickCloseButton()
+            }
+        }
+    }
+
+    @Test
+    fun verifyIncorrectCredentialsError() = run {
+        step("Verify error message when there are incorrect credentials, email: ${SignInData_Incorrect.email} password: ${SignInData_Incorrect.password}"){
+            AuthenticationScreen {
+                authenticate(SignInData_Incorrect.email, SignInData_Incorrect.password)
+            }
+            AndroidDialog {
+                verifyDialogMessage("The password is invalid or the user does not have a password.")
                 clickCloseButton()
             }
         }
