4a286c5fee81bf4c991bba4129104d828bf52d25
Jakub Cekała <75976627+jakubcekala@users.noreply.github.com>
2024-04-02 11:32:51 +0200

    Setup Allure report (#5)

--- ShoppingApp/app/src/androidTest/java/com/shopping/app/tests/BaseTest.kt
+++ ShoppingApp/app/src/androidTest/java/com/shopping/app/tests/BaseTest.kt
@@ -3,6 +3,8 @@ package com.shopping.app.tests
 import androidx.test.core.app.ActivityScenario
 import androidx.test.platform.app.InstrumentationRegistry
 import com.google.firebase.auth.FirebaseAuth
+import com.kaspersky.components.alluresupport.withForcedAllureSupport
+import com.kaspersky.kaspresso.kaspresso.Kaspresso
 import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
 import com.shopping.app.data.preference.UserPref
 import com.shopping.app.ui.MainActivity
@@ -12,7 +14,7 @@ import kotlinx.coroutines.launch
 import org.junit.After
 import org.junit.Before
 
-open class BaseTest : TestCase() {
+open class BaseTest : TestCase(kaspressoBuilder = Kaspresso.Builder.withForcedAllureSupport()) {
 
     val context = InstrumentationRegistry.getInstrumentation().context
 
